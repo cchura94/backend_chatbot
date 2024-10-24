a288b85e93060033772160c18345b4fb28deb822
cristian <cchura.cpc@gmail.com>
2024-10-23 21:36:41 -0400

    base de datos

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require("sequelize");
+require('dotenv').config() // process.env.PORT
 
-const sequelize = new Sequelize(process.env.BD_NAME, process.env.BD_USER, process.env.BD_PASS, {
-  host: process.env.BD_HOST,
+const sequelize = new Sequelize(process.env.BD_NAME || 'chatbot_node', process.env.BD_USER || 'postgres', process.env.BD_PASS || 'postgresql', {
+  host: process.env.BD_HOST || 'localhost',
   dialect: "postgres",
-  port: process.env.PORT || 5432
+  port: process.env.BD_PORT || 5433
 });
 
 module.exports = sequelize
